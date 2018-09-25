235e5ac10b307c9072363c0c3d4f364dc3775463
Ken Maglio <ken.maglio@wwt.com>
2018-09-25 15:30:31 -0500

    Adds missing functions to LTM

--- ltm.go
+++ ltm.go
@@ -1502,6 +1502,10 @@ func (b *BigIP) Nodes() (*Nodes, error) {
 	return &nodes, nil
 }
 
+func (b *BigIP) AddNode(config *Node) error {
+	return b.post(config, uriLtm, uriNode)
+}
+
 // CreateNode adds a new node to the BIG-IP system.
 func (b *BigIP) CreateNode(name, address string) error {
 	config := &Node{
@@ -1885,6 +1889,16 @@ func (b *BigIP) VirtualAddresses() (*VirtualAddresses, error) {
 		return nil, err
 	}
 	return &va, nil
+}
+
+// GetVirtualAddress retrieves a VirtualAddress by name. Returns nil if the VirtualAddress does not exist
+func (b *BigIP) GetVirtualAddress(vaddr string) (*VirtualAddress, error) {
+	var virtualAddress VirtualAddress
+	err, _ := b.getForEntity(&virtualAddress, uriLtm, uriVirtualAddress, vaddr)
+	if err != nil {
+		return nil, err
+	}
+	return &virtualAddress, nil
 }
 
 func (b *BigIP) CreateVirtualAddress(vaddr string, config *VirtualAddress) error {
