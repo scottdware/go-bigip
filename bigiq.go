32efc1ed66ba97c61b630781154ace5f32a0e82b
dinesh <d.papineni@f5.com>
2020-11-25 19:34:41 +0530

    Adding bigiq vendor changes

--- bigiq.go
+++ bigiq.go
@@ -20,6 +20,7 @@ const (
 	uriMembers     = "members"
 	uriTasks       = "tasks"
 	uriManagement  = "member-management"
+	uriPurchased   = "purchased-pool"
 )
 
 type BigiqDevice struct {
@@ -203,6 +204,15 @@ func (b *BigIP) GetPoolType(poolName string) (*regKeyPool, error) {
 			return &pool, nil
 		}
 	}
+        err, _ = b.getForEntity(&self, uriMgmt, uriCm, uriDevice, uriLicensing, uriPool, uriPurchased, uriLicenses)
+        if err != nil {
+                 return nil, err
+         }
+         for _, pool := range self.RegKeyPoollist {
+                 if pool.Name == poolName {
+                         return &pool, nil
+                 }
+         }
 	return nil, nil
 }
 
